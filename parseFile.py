3dc5ca79f11152d58eed4f86658ca87d9862a006
meet16 <meetkpatel16@gmail.com>
2018-03-02 23:19:36 +0530

    updated file to get ipaddress in unix enviroment

--- parseFile.py
+++ parseFile.py
@@ -5,6 +5,16 @@ def parse():
     print("IP address :",socket.gethostbyname(socket.gethostname())+":8000")
     cwd = os.getcwd()  # used by static file server
     print("Current Working directory :" + cwd)
+
+    ipaddress = ""
+    if (os.name == "nt"):
+        ipaddress = socket.gethostbyname(socket.gethostname())
+    else:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ipaddress = s.getsockname()[0]
+        s.close()
+
     onlyfiles = [f for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
     for htmlFile in onlyfiles:
         if htmlFile.endswith(".html"):
@@ -17,7 +27,7 @@ def parse():
                     arr = line.split(":")
                     str = arr[0]
                     str += "://"
-                    str += socket.gethostbyname(socket.gethostname())
+                    str += ipaddress
                     str += ":"
                     str += arr[2]
                     filewrite += str
