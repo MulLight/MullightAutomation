e80094ba9593a8f94780122b5f7496267c96ae65
Meet Patel <meetkpatel16@gmail.com>
2018-03-04 21:14:09 +0530

    Updated init.sh by adding dependency

--- init.sh
+++ init.sh
@@ -2,4 +2,12 @@ git stash
 git stash clear
 git pull
 
-python3 main.py &
+pip3 install tornado
+pip3 install motor
+pip3 install xlsxwriter
+pip3 install openpyxl
+pip3 install serial
+pip3 install pymongo
+
+
+sudo python3 main.py &
