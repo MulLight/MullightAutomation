2a6273beca6d065a375d016d6386e26203dfb756
Meet Patel <meetkpatel16@gmail.com>
2018-03-06 16:35:43 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,52 @@
+#!/bin/bash
+
+connectivity=`ifconfig | grep -c "192"`
+if [ $connectivity -lt 1 ]
+then 
+echo "Not connected"
+fi
+
 git stash
 git stash clear
 git pull
 
+pip3list=`pip3 list`
+
+tornado=`echo $pip3list | grep -c tornado`
+if [ $tornado -lt 1 ]
+then
 pip3 install tornado
+fi
+
+motor=`echo $pip3list | grep -c motor`
+if [ $motor -lt 1 ]
+then
 pip3 install motor
+fi
+
+xlsxwriter=`echo $pip3list | grep -c xlsxwriter`
+if [ $xlsxwriter -lt 1 ]
+then
 pip3 install xlsxwriter
+fi
+
+openpyxl=`echo $pip3list | grep -c openpyxl`
+if [ $openpyxl -lt 1 ]
+then
 pip3 install openpyxl
+fi
+
+serial=`echo $pip3list | grep -c serial`
+if [ $tornado -lt 1 ]
+then
 pip3 install serial
+fi
+
+pymongo=`echo $pip3list | grep -c pymongo`
+if [ $tornado -lt 1 ]
+then
 pip3 install pymongo
+fi
 
 
 sudo python3 main.py &
